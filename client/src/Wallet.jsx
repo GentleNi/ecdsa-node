84512552dc81c7cb009db3fd0b2d7bc78648fb4e
GentelNi <562391834@qq.com>
2023-07-04 13:54:53 +0800

    final

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -2,10 +2,18 @@ import * as secp from "ethereum-cryptography/secp256k1";
 import { toHex } from "ethereum-cryptography/utils";
 import server from "./server";
 
+const privateKeys = [
+  "01d05ffbe92969d222ec4b30263b5fa2795c398c5e1b6164eacb5c7d5537a320",
+  "fd6025bb479eefbf5563c8f4aca62f8f935e811d9f2a562659d070363203be47",
+  "4ea678434a9a036fa3fb00d5113ff93c9e2dd89116d98d509f1694c322591d57"
+]
 
 function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey}) {
   async function onChange(evt) {
-    console.log(secp.get)
+    // const priKey = toHex(secp.utils.randomPrivateKey());
+    // console.log(priKey);
+    // console.log(toHex(secp.getPublicKey(priKey)));
+
     const newPrivateKey = evt.target.value;
     setPrivateKey(newPrivateKey);
     const publicKey = toHex(secp.getPublicKey(newPrivateKey));
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const secp = require("ethereum-cryptography/secp256k1");
+const {
+  toHex,
+  utf8ToBytes,
+  hexToBytes,
+} = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "04866567d24aea3432c9e6174e7ab52d1d74db92cf3f892da27cce2258e164d62dffc7da8cfb0fa0f8da537dd52dcf45a12b28145f1d1a5519ed24b3bff732d7d3": 100,
+  "042d83c8fc4d4760c1d38a411232c4e285d19c72a3c4b5910ee46fa90a534e24d530ca0ef8ad54f7b1bb6060347deeb24c7a2dd76328120f54d86a30fd28ccf728": 50,
+  "04680734f689149a8f1e86befb37d3435986b317a81f9da206f38fcc45c3f33369b5386997c43703224704a07bb1740b140cbddb9f32c69be376d0b87fade2967a": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -22,6 +29,12 @@ app.post("/send", (req, res) => {
   const { message, signature, recoveryBit } = req.body;
   const { recipient, amount } = message;
   const messageHash = toHex(keccak256(utf8ToBytes(JSON.stringify(message))));
+  const publicKey2 = secp.recoverPublicKey(
+    messageHash,
+    hexToBytes(signature),
+    recoveryBit
+  );
+  const sender = toHex(publicKey2);
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
